e6ec580e8e9083c0b4d65441b11258c4c4c7134d
xaviizq <gtuusustu@gmail.com>
2024-03-20 20:21:39 +0100

    Primer commit

---
+++ php-attendance/pages/home.php
@@ -0,0 +1,3 @@
+<div class="page-title">Practica Alex</div>
+<hr>
+<img src="https://www.the-sun.com/wp-content/uploads/sites/6/2023/10/KS-crunchy-cat-comp.jpg?w=620" alt="Descripción de la imagen">
